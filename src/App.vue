6cfa6700fa2bd3669258a6a5a10e1b557875dc33
changxianglin <281532612@qq.com>
2021-03-12 02:17:23 +0800

    use proxy

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <h1>{{count}}</h1>
   <h1>{{double}}</h1>
+  <ul>
+    <li v-for="number in numbers" :key="number">
+      <h1>{{number}}</h1>
+    </li>
+  </ul>
+  <h1>{{person.name}}</h1>
   <button @click="increase">+1</button>
 </template>
 
@@ -11,6 +17,8 @@ interface DataProps {
   count: number;
   double: number;
   increase: () => void;
+  numbers: number[];
+  person: {name?: string};
 }
 export default {
   name: 'App',
@@ -25,8 +33,12 @@ export default {
     const data: DataProps = reactive({
       count: 0,
       increase: () => { data.count++ },
-      double: computed(() => data.count * 2)
+      double: computed(() => data.count * 2),
+      numbers: [0, 1, 2],
+      person: {},
     })
+    data.numbers[0] = 5
+    data.person.name = 'xianglin'
     const refData = toRefs(data)
     return {
       ...refData,
